53b316c7e0f69d925cdbd58aaa179fb6640145da
jjunhaa0211 <goodjunha@gmail.com>
2024-08-07 14:36:24 +0900

    Initial Commit

---
+++ Tests/JBottonSheetTests/JBottonSheetTests.swift
@@ -0,0 +1,12 @@
+import XCTest
+@testable import JBottonSheet
+
+final class JBottonSheetTests: XCTestCase {
+    func testExample() throws {
+        // XCTest Documentation
+        // https://developer.apple.com/documentation/xctest
+
+        // Defining Test Cases and Test Methods
+        // https://developer.apple.com/documentation/xctest/defining_test_cases_and_test_methods
+    }
+}
